c3ae3baf4de8f4561e5c86d00267e84540e98541
Zeger-Jan van de Weg <git@zjvandeweg.nl>
2018-09-20 21:51:44 +0200

    Update protoc for improved imports
    Protoc didn't generated the right code, which made the Go code not
    compiling. A few google searches further, I noticed that bumping
    probably would solve it.
    
    Given this branch is an experiment still, only my system is supported at
    this time.

--- go/gitalypb/shared.pb.go
+++ go/gitalypb/shared.pb.go
@@ -7,7 +7,7 @@ import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 import google_protobuf "github.com/golang/protobuf/ptypes/timestamp"
-import google_protobuf1 "google/protobuf"
+import google_protobuf1 "github.com/golang/protobuf/protoc-gen-go/descriptor"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
